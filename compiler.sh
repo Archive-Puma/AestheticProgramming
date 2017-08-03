b03a7b9035a5b4acf12aebdbcde7a895896923cb
CosasDePuma <kikedesarrollador@gmail.com>
2017-08-03 22:30:05 +0200

    Compiles to x86, only

--- compiler.sh
+++ compiler.sh
@@ -3,45 +3,36 @@
 if [ $# == '1' ]; then
     echo '[?] Compiling ASM using NASM...'
     #NASM
-    if [ `getconf LONG_BIT` == '64' ]; then
-        echo '[?] Kernel: x86_64 (64-bit kernel)'
-        nasm -f elf64 $1.asm
-    else
-        echo '[?] Kernel: i686 (32-bit kernel)'
-        nasm -f elf $1.asm
-    fi
+    nasm -f elf $1.asm
     if [ $? != '0' ]; then
         echo '[!] NASM Error!'
         exit
     fi
     #LD
-    ld -s -o $1 $1.o
+    ld -m elf_i386 -s -o $1 $1.o
     if [ $? != '0' ]; then
         echo '[!] LD Error!'
         exit
     fi
+    #JUNK
     rm -rf $1.o
     echo '[?] SUCCESSFULLY COMPILED!'
+
 elif [ $# == '2' ]; then
     echo '[?] Compiling ASM using NASM...'
     #NASM
-    if [ `getconf LONG_BIT` == '64' ]; then
-        echo '[?] Kernel: x86_64 (64-bit kernel)'
-        nasm -f elf64 $1 -o tmp.o
-    else
-        echo '[?] Kernel: i686 (32-bit kernel)'
-        nasm -f elf $1 -o tmp.o
-    fi
+    nasm -f elf $1 -o tmp.o
     if [ $? != '0' ]; then
         echo '[!] NASM Error!'
         exit
     fi
     #GCC
-    ld -s -o $2 tmp.o
+    ld -m elf_i386 -s -o $2 tmp.o
     if [ $? != '0' ]; then
         echo '[!] LD Error!'
         exit
     fi
+    #JUNK
     rm -rf tmp.o
     echo '[?] SUCCESSFULLY COMPILED!'
 else 
