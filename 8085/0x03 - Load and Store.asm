0d7272b5ac1295b03c47c2c1378b80fab8deaf16
Cosas de Puma <kikefontanlorenzo@gmail.com>
2017-09-23 01:42:28 +0200

    LD and ST

---
+++ 8085/0x03 - Load and Store.asm
@@ -0,0 +1,17 @@
+; MVI - Set the value of a Register
+
+MVI A,DB
+MVI B,EE
+MVI C,AE
+MVI D,DF
+
+; MOV - Copy the value of a Register to another
+
+MOV E, A
+MOV H, B
+MOV L, C
+MOV M, D
+
+; HLT - Stop the program
+
+HLT
