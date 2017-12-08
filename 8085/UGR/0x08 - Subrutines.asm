83b7f46b67f1974cc92ea0c66c11e01e20dca114
CosasDePuma <kikefontanlorenzo@gmail.com>
2017-12-08 19:32:44 +0100

    0x08

---
+++ 8085/UGR/0x08 - Subrutines.asm
@@ -0,0 +1,28 @@
+; Locate code in this address
+.org E000h
+
+; Call the subrutines
+CALL CLEAR
+CALL VALUES
+
+; Stop the program
+HLT
+
+; Locate subrutines in this address
+.org AAAAh
+
+; Create CLEAR subrutine
+CLEAR:
+  XRA A
+  LXI B,0000H
+  LXI D,0000H
+  LXI H,0000H
+  RET
+
+; Create VALUES subrutine
+VALUES:
+  MVI A,12h
+  LXI B,CAFEh
+  LXI D,CEB0h
+  LXI H,B0CAh
+  RET
