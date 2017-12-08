f53657038c01dbb68103d340c23d211d9c331b37
CosasDePuma <kikefontanlorenzo@gmail.com>
2017-12-08 19:07:42 +0100

    0x01

---
+++ 8085/UGR/0x01 - Move.asm
@@ -0,0 +1,17 @@
+; Locate code in this address
+.org E000h
+
+; MVI - Set the value of a Register
+MVI A,DBh
+MVI B,EEh
+MVI C,AEh
+MVI D,DFh
+
+; MOV - Copy the value of a Register to another
+MOV E,A
+MOV H,B
+MOV L,C
+MOV M,D
+
+; HLT (76) - Stop the program
+HLT
